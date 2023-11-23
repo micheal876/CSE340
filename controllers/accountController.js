0756b8cd54dbe1f68c41260cb9d30bc30613f14d
micheal876 <edw20011@byui.edu>
2023-11-22 20:32:24 -0500

    val done

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -27,6 +27,35 @@ async function buildRegister(req, res, next) {
 }
 
 
+/* ****************************************
+*  Process Login
+* *************************************** */
+async function logToAccount(req, res) {
+  let nav = await utilities.getNav()
+  const { account_email, account_password } = req.body
+  const accountData = await accountModel.getAccountByEmail(account_email)
+  if (!accountData) {
+    req.flash("notice", "Please check your credentials and try again.")
+    res.status(400).render("account/login", {
+      title: "Login",
+      nav,
+      errors: null,
+      account_email,
+    })
+    return
+  }
+  try {
+    if (await bcrypt.compare(account_password, accountData.account_password)) {
+      delete accountData.account_password
+      const accessToken = jwt.sign(accountData, process.env.ACCESS_TOKEN_SECRET, { expiresIn: 3600 * 1000 })
+      res.cookie("jwt", accessToken, { httpOnly: true, maxAge: 3600 * 1000 })
+      return res.redirect("/account/")
+      }
+    } catch (error) {
+      return new Error('Access Forbidden')
+  }
+}
+
 
 
 /* ****************************************
@@ -81,4 +110,6 @@ async function registerAccount(req, res) {
 }
 
 
-module.exports = { buildLogin, buildRegister, registerAccount }
+
+
+module.exports = { buildLogin, buildRegister, registerAccount, logToAccount}
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,17 +3,28 @@ const express = require("express")
 const router = new express.Router() 
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities")
+const regValidate = require('../utilities/account-validation')
+
+
 
 // Route to build pages
-
-
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
 router.get("/register", utilities.handleErrors(accountController.buildRegister));
 
+// Process the registration data
 router.post(
     "/register",
+    regValidate.registrationRules(),
+    regValidate.checkRegData,
     utilities.handleErrors(accountController.registerAccount)
 )
 
+// Process the login attempt
+router.post(
+    "/login",
+    (req, res) => {
+      res.status(200).send('login process')
+    }
+  )
 module.exports = router;
